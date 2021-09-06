d74cca1496d2ea5ed95caa9a927bac0799440a97
nethmaa <it20132446@my.sliit.lk>
2021-09-06 22:10:22 +0530

    add spme files

--- rental_frontend/src/App.js
+++ rental_frontend/src/App.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import AdminContainer from './components/admin/AdminContainer';
+import Footer from './components/shared/footer/Footer';
+import AllPackagesForUser from './components/packages/all_packages/package_views/user/AllPackagesForUser';
+
+import ViewMorePackage from './components/packages/all_packages/ViewMorePackage';
+
+import AdminNavbar from './components/shared/navigators/AdminNavbar';
+
 function App() {
+
   return (
-    <div className="App">
-      <h1>Hellow</h1>
-    </div>
+    <React.Fragment>
+      <Router>
+        <AdminNavbar />
+        <Switch>
+          <Route path="/admin">
+            <AdminContainer />
+          </Route>
+          <Route
+            path="/packages/:id"
+            component={(props) => (
+              <ViewMorePackage {...props} key={window.location.pathname} />
+            )}
+          />
+       
+          <Route exact path="/">
+            <AllPackagesForUser />
+            
+          </Route>
+        </Switch>
+      </Router>
+
+      <Footer />
+
+    </React.Fragment>
   );
 }
 
 export default App;
+
